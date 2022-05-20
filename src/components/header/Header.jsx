beb5bd816e2b0f30e59310869c07f07c98a827c1
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2022-05-19 20:36:00 -0500

    Boriqua Bros Website
    Started Project using React as frontend.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='container header__container'>
+            <h5>Welcome to</h5>
+            <h1>Boriqua Bros</h1>
+        </div>
+    </header>
+    )
+}
+
+export default Header
