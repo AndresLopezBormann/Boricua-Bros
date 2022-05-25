fb4d455ef0b134bd5f9165db2d57ce7af56cb9bc
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2022-05-24 20:55:52 -0500

    Added menu page and changed the h5 and h2 tags
    The h5 and h2 tags were in my opinion too similar to the backround so i changed them so that they would end up being green.
    
    The menu page only has fried plantains but i'll work on other puerto rican food when i have a sense of what the project will look like

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import Plantain from './../../assets/plantain.jpg'
 import './Menu.css'
 
 const Menu = () => {
   return (
-    <section id='menu'>Menu</section>
+    <section id='menu'>
+      <h5>Take A Look At Some Of My Work</h5>
+      <h2>Menu</h2>  
+      <div className="container menu__container">
+        <article className='menu__item'>
+          <div className="menu__item-image">
+            <img src={Plantain} alt=""/>
+          </div>
+            <h3>Plantains</h3>
+            <div className='menu__item-CTA'><a href='https://www.healthline.com/health/food-nutrition/plantain-nutrition-benefits' target="_blank" className='btn'>Nutritional Facts</a>
+            <a href='https://www.thespruceeats.com/fried-ripe-plantains-recipe-2138201' target="_blank" className='btn btn-primary'>How it is prepared</a>
+            </div> 
+        </article>
+
+        <article className='menu__item'>
+          <div className="menu__item-image">
+            <img src={Plantain} alt=""/>
+          </div>
+            <h3>Plantains</h3>
+            <div className='menu__item-CTA'><a href='https://www.healthline.com/health/food-nutrition/plantain-nutrition-benefits' target="_blank" className='btn'>Nutritional Facts</a>
+            <a href='https://www.thespruceeats.com/fried-ripe-plantains-recipe-2138201' target="_blank" className='btn btn-primary'>How it is prepared</a>
+            </div> 
+        </article>
+
+        <article className='menu__item'>
+          <div className="menu__item-image">
+            <img src={Plantain} alt=""/>
+          </div>
+            <h3>Plantains</h3>
+            <div className='menu__item-CTA'><a href='https://www.healthline.com/health/food-nutrition/plantain-nutrition-benefits' target="_blank" className='btn'>Nutritional Facts</a>
+            <a href='https://www.thespruceeats.com/fried-ripe-plantains-recipe-2138201' target="_blank" className='btn btn-primary'>How it is prepared</a>
+            </div> 
+        </article>
+
+        <article className='menu__item'>
+          <div className="menu__item-image">
+            <img src={Plantain} alt=""/>
+          </div>
+            <h3>Plantains</h3>
+            <div className='menu__item-CTA'><a href='https://www.healthline.com/health/food-nutrition/plantain-nutrition-benefits' target="_blank" className='btn'>Nutritional Facts</a>
+            <a href='https://www.thespruceeats.com/fried-ripe-plantains-recipe-2138201' target="_blank" className='btn btn-primary'>How it is prepared</a>
+            </div> 
+        </article>
+
+      </div>
+    </section>
   )
 }
 
 export default Menu
+
