23ec62383ab71e27ec4f77b125bd7ba13cf9efcf
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2022-06-09 17:21:35 -0500

    Bug fixes
    Added transition speed, got rid of height in each section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href='#locations' onClick={() => setActiveNav('#locations')} className={activeNav === '#locations' ? 'active' : ''}><BiMap/></a>
       <a href='#menu' onClick={() => setActiveNav('#menu')} className={activeNav === '#menu' ? 'active' : ''}><GiMeal/></a>
-      <a href='#aboutus' onClick={() => setActiveNav('#aboutus')} className={activeNav === '#aboutus' ? 'active' : ''}><HiOutlineAtSymbol/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><HiOutlineAtSymbol/></a>
       <a href='#testimonials' onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><FaRegSmileBeam/></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><VscCallOutgoing/></a>
     </nav>
