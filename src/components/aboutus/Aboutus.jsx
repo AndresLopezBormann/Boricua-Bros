19837b675240eae420334eb4952c1f326e5c9132
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2022-06-09 17:18:36 -0500

    Finished About Us page
    Created and finished the about us page so that all I have to do now is add the info

--- src/components/aboutus/Aboutus.jsx
+++ src/components/aboutus/Aboutus.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
-import './Aboutus.css'
+import "./Aboutus.css"
+import { FiAward } from 'react-icons/fi'
 
-const Aboutus = () => {
+import Plantain from '../../assets/plantain.jpg'
+const About = () => {
   return (
-    <section id='aboutus'>About Us</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2> About Us</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={Plantain} alt =""/>
+          </div>
+        </div>
+        <div className='about__content'>
+        <div className="about__cards">
+          <article className='about__card'>
+            <FiAward className='about__icon'/>
+            <h5>Restaurant Award</h5>
+            <small>Place to celebrate and show off Awards</small>
+          </article>
+          <article className='about__card'>
+            <FiAward className='about__icon'/>
+            <h5>Restaurant Award</h5>
+            <small>Place to celebrate and show off Awards</small>
+          </article>
+          <article className='about__card'>
+            <FiAward className='about__icon'/>
+            <h5>Restaurant Award</h5>
+            <small>Place to celebrate and show off Awards</small>
+          </article>
+        </div>
+
+        <p>Hello! My name is Andres Lopez-Bormann.</p>
+        <a href='#contact' className='btn btn-primary'>Let't Talk</a>
+        </div>
+      </div>
+        
+    </section>
+    
   )
 }
 
-export default Aboutus
+export default About
